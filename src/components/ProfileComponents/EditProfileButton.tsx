a2d4289655fb6ec619b5f96115f0ec0c4c04fdaa
citrix <melissaruizdelg21@gmail.com>
2025-12-01 01:29:47 -0500

    pruebita

---
+++ src/components/ProfileComponents/EditProfileButton.tsx
@@ -0,0 +1,9 @@
+import "./../../GlobalObjects/Global.css"
+const EditProfileButton = () => {
+    return (
+        <button className=" icon carousel-button d-flex justify-content-center align-items-center border-0">
+            <i className="bi bi-pencil-square"></i>
+        </button>
+    )
+}
+export default EditProfileButton;
